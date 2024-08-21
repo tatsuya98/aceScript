7a2f99cb5f54431cc4c64e7b656b6753a2d56bef
Hydrochilli <mainzer10@gmail.com>
2024-08-21 13:40:08 +0100

    adds user-profile and page

--- code-ninjas/app/profile/page.tsx
+++ code-ninjas/app/profile/page.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-  return <div>Profile</div>;
-}
+"use client"
+import React from 'react';
+import UserProfile from './UserProfile';
+
+const dummyData = {
+  avatarUrl: '', //  <-----if empty string defaults to default avatar.
+  username: 'JohnDoe',
+
+};
+
+const ProfilePage: React.FC = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+      <UserProfile
+        avatarUrl={dummyData.avatarUrl}
+        username={dummyData.username}
+    
+      />
+    </div>
+  );
+};
+
+export default ProfilePage;
