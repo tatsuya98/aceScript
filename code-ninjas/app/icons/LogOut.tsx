2ca17c56c664572e9efb0f8cfbc01c0721d5bc2b
tatsuya98 <nathanbailey_62@hotmail.com>
2024-08-22 15:38:07 +0100

    completed login register profile

---
+++ code-ninjas/app/icons/LogOut.tsx
@@ -0,0 +1,19 @@
+export default function LogOut() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="24"
+      height="24"
+      xmlSpace="preserve"
+      version="1.1"
+      viewBox="0 0 24 24"
+    >
+      <image
+        width="24"
+        height="24"
+        xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAhJJREFUSEvFlk9r4kAUwGfSoHQVwT2sB91r8SZbEXoxCLuLqUpC4sHvsH4L+y0EW6ieRNyIFjafwaV4EIOIFhGkqPXSdUHXbMoLGRHrn7YwdG4z8977vX95E4woL0zZPtoGgLOjLWAdIWRY50SP7Hf6uQlgEEL/XxEV6O+FrANMYY/H80kQBNHr9foYhgGgufL5/FWn0+khZEZ9bB3/RQjtdYoATKFQKBRSVVV1u91uYtgwDANjjHmeP1dV9RecZ7PZLMdxXDgcjozH43srpZDCZwsABML2er32YrFYiKIojkaje4zxKvzpdPpIDFUqFUUQBKHZbDZjsVh0MBgMd0HAuOm93+8/0TStnUqlUsVisbivDqVSqSRJkgQpbLVaLZ7nv++CkI7Rg8Hgab1e/y2KolStVquWR/9cLtdHAnM6ncxsNtMLhcJ1IpFIzOfzud1ut2uapkWj0W/bICtAIBD40mg0bpPJpFwul3+C0Uwmc5FOp3/ouq6zLMsSkMPhcNhsNhvsl8vlEu42IKvCPwNA6IqiKKBcq9Vu4vF47FDbEgikKxKJfLUKb0L2AiDXsizLxAABQVdtQolMt9vtchx3NhwOH0DmfQFUUkStyDTblPqHdnBUMAxjTCaTP28dFdBNVIfdqr3Xx7XP5/u83u+5XO6y3+/fWa39wVKavXRcEwjVB2c9EmpP5qGx8+p76n8VT1GFnypFXpcKAAAAAElFTkSuQmCC"
+      />
+    </svg>
+  );
+}
